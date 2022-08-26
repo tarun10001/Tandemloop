5c0928e3f308c0765e4260940828edf1c92783f5
Tarun Kumawat <97452134+tarun10001@users.noreply.github.com>
2022-08-26 19:35:05 +0530

    Add files via upload

---
+++ Problem-1 Calculator/src/App.js
@@ -0,0 +1,75 @@
+import "./App.css";
+import { useState } from "react";
+
+
+function App() {
+
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ["/", "*", "+", "-", "."];
+
+  const updateCalc = (value) => {
+    if (
+      (ops.includes(value) && calc === '') ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+  };
+
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  }
+
+
+  const reset = () => {
+    return setCalc("") + setResult("");
+  }
+
+
+  return (
+
+    <div className="App">
+      <h1>React Calculator</h1>
+      <div className="calculator">
+      
+        <div className="display">
+          {result ? <span>({result})</span> : ""}&nbsp;
+          {calc || "0"}
+        </div>
+
+        <div className="operators">
+          <button onClick={() => updateCalc("/")}>/</button>
+          <button onClick={() => updateCalc("*")}>*</button>
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+
+          <button style={{backgroundColor:"red"}} onClick={reset}>CLR</button>
+        </div>
+
+
+        <div className="digits">
+          <button onClick={() => updateCalc("7")}>7</button>
+          <button onClick={() => updateCalc("8")}>8</button>
+          <button onClick={() => updateCalc("9")}>9</button>
+          <button onClick={() => updateCalc("4")}>4</button>
+          <button onClick={() => updateCalc("5")}>5</button>
+          <button onClick={() => updateCalc("6")}>6</button>
+          <button onClick={() => updateCalc("1")}>1</button>
+          <button onClick={() => updateCalc("2")}>2</button>
+          <button onClick={() => updateCalc("3")}>3</button>
+
+
+          <button onClick={() => updateCalc("0")}>0</button>
+          <button onClick={() => updateCalc(".")}>.</button>
+
+          <button onClick={calculate}>=</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
